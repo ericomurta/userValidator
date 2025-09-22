e92f2b2eed0a036779f8ea1fd937cbe4ca7aeeee
mohamedlc <mohamed.capistrano@gmail.com>
2025-09-22 17:46:22 -0300

    feat(databaseConfiguration): realizado o seed do banco de dados H2

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_user(name, mail) VALUES ('Joao', 'joao@email.com');
+INSERT INTO tb_user(name, mail) VALUES ('Maria', 'maria@email.com');
+INSERT INTO tb_user(name, mail) VALUES ('Paulo', 'paulo@email.com');
